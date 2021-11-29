99c62edd0f791301df29738add33c18eec2aa399
Pooja SAXENA <nrjrasaxena@gmail.com>
2021-11-29 17:33:38 +0100

    corrected the video path

--- facial-expression-recognizition-keras/camera.py
+++ facial-expression-recognizition-keras/camera.py
@@ -4,12 +4,12 @@ import numpy as np
 
 facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 model = FacialExpressionModel("model.json", "model_weights.h5")
-font = cv2.FONT_HERSHEY_SIMPLEX
+font  = cv2.FONT_HERSHEY_SIMPLEX
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture('/home/rhyme/Desktop/Project/videos/facial_exp.mkv')
-
+        #self.video = cv2.VideoCapture('/home/rhyme/Desktop/Project/videos/facial_exp.mkv')
+        self.video = cv2.VideoCapture('/Users/psaxena/Documents/weitebildung/face_recognition_modelling/facial-expression-recognizition-keras/videos/facial_exp.mkv')
     def __del__(self):
         self.video.release()
 
